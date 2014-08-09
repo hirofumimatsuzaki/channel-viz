4c7df681a04c26c7c8e71df0400e202072b92d41
hirofumimatsuzaki <hirofumimatsuzaki@gmail.com>
2014-08-09 10:15:34 +0900

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,7 +12,7 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
+	var defaultKey		= '9vRV7RCQOKdTaErxjXM5ci7iWwUGJgYH2yc1aAQqPOlZu0Mz', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
 		applicationName	= '', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
