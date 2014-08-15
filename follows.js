824fc5e7afbec13414048cd0880cec29182be717
hirofumimatsuzaki <hirofumimatsuzaki@gmail.com>
2014-08-15 22:00:54 +0900

    Update follows.js

--- follows.js
+++ follows.js
@@ -12,7 +12,7 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= 'UojuY11aB8WHsuHTGqaLMCeukODnBCvaV', // Unique master Xively API key to be used as a default
+	var defaultKey		= 'UojuY11aB8WHsuHTGqaLMCeukODnBCvaVFo18T73f6T1Up06', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [2035121252], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Studio Kura Making Room', // Replaces Xively logo in the header
 		dataDuration	= '10800', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
